b56c7b3d7e21023d920665ea419cec6c6d89d27a
Dirk Brünsicke <d1rk@gmx.de>
2010-06-03 14:28:54 +0200

    added file and folder

--- support.php
+++ support.php
@@ -3,4 +3,6 @@ define('ROOT', dirname(dirname(__FILE__)));
 @include_once('libs/basics.php');
 @include_once('libs/svn.php');
 @include_once('libs/configure.php');
+@include_once('libs/folder.php');
+@include_once('libs/file.php');
 ?>
